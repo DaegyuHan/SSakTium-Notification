6b82d9b13c121edfa9ad505e3ead56b64a55938e
Daegyu Han <bigstar77han@gmail.com>
2025-04-09 18:56:37 +0900

    [FEAT] API Gateway 연결로 구독 시 메인서버 접근 제거

--- src/main/java/com/sparta/notification/domain/notifications/controller/NotificationController.java
+++ src/main/java/com/sparta/notification/domain/notifications/controller/NotificationController.java
@@ -1,18 +1,12 @@
 package com.sparta.notification.domain.notifications.controller;
 
-import com.sparta.notification.domain.common.AuthTokenHolder;
 import com.sparta.notification.domain.notifications.client.UserClient;
-import com.sparta.notification.domain.notifications.entity.Notification;
 import com.sparta.notification.domain.notifications.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,21 +17,15 @@ public class NotificationController {
     private final UserClient userClient;
 
     @GetMapping("/v1/notifications/subscribe")
-    public SseEmitter subscribe(@RequestHeader("Authorization") String authHeader) {
-        log.info("🔔 구독 완료");
+    public SseEmitter subscribe(
+            @RequestHeader("X-User-Id") Long userId
+    ) {
+        log.info("🔔 구독 요청 - userId: {}", userId);
 
-        // 토큰 저장
-        AuthTokenHolder.setToken(authHeader);
-
-        // 이후 서비스 로직
-        Long userId = userClient.getMyInfo().getUserId();
         SseEmitter emitter = notificationService.subscribe(userId);
 
         // 미확인 알림 전송
         notificationService.sendUnreadNotifications(userId);
-
-        // ThreadLocal 정리
-        AuthTokenHolder.clear();
 
         return emitter;
     }
