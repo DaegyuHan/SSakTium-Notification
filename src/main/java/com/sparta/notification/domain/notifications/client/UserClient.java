56b03513cd2af503cfb5ba0d2f1b3596b1f53f8c
Daegyu Han <bigstar77han@gmail.com>
2025-04-10 04:32:02 +0900

    [FEAT] 팔로워 리스트 요청 Feign Client api 추가

--- src/main/java/com/sparta/notification/domain/notifications/client/UserClient.java
+++ src/main/java/com/sparta/notification/domain/notifications/client/UserClient.java
@@ -4,10 +4,15 @@ import com.sparta.notification.config.FeignClientConfig;
 import com.sparta.notification.domain.notifications.dto.UserInfoResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "userClient", url = "${user-service.url}", configuration = FeignClientConfig.class)
 public interface UserClient {
     @GetMapping("/v1/api/internal/users/me")
     UserInfoResponseDto getMyInfo();
+
+    @GetMapping("/v1/api/internal/friends/{userId}/followers/ids")
+    List<Long> getFollowerIds(@PathVariable("userId") Long userId);
 }
--- src/main/java/com/sparta/notification/domain/notifications/entity/EventType.java
+++ src/main/java/com/sparta/notification/domain/notifications/entity/EventType.java
@@ -3,6 +3,6 @@ package com.sparta.notification.domain.notifications.entity;
 public enum EventType {
     FRIEND_REQUESTED,
     FRIEND_ACCEPTED,
-    FRIEND_BOARD,
+    POST_CREATED,
     PLANT_MANAGE
 }
